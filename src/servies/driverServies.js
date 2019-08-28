352d6ebb4dd1bbe8ccaca914e4aa383436f34a0b
Yan.Palyn <Kvinto1986@gmail.com>
2019-08-28 12:26:36 +0300

    add create warehouse form

--- src/servies/driverServies.js
+++ src/servies/driverServies.js
@@ -9,7 +9,7 @@ export const addDriver = (driver) => {
         name: name,
         surname:  surnName,
         drivingLicense: drivingLicense
-    } )
+    })
 };
 
 export const findDriver = (data) => {
